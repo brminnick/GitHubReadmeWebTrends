0eed732ea0fce50cad68b8eb5114213cf0588e8e
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2020-09-20 12:28:33 -0700

    Update Logging & DI

--- GitHubReadmeWebTrends.OptOutSite/Program.cs
+++ GitHubReadmeWebTrends.OptOutSite/Program.cs
@@ -1,9 +1,11 @@
 using System;
 using System.Net.Http;
 using System.Threading.Tasks;
+using Blazor.Extensions.Logging;
 using GitHubReadmeWebTrends.Common;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace GitHubReadmeWebTrends.OptOutSite
 {
@@ -15,6 +17,7 @@ namespace GitHubReadmeWebTrends.OptOutSite
             builder.RootComponents.Add<App>("app");
 
             builder.Services.AddSingleton<OptOutDatabase>();
+            builder.Services.AddLogging(builder => builder.AddBrowserConsole().SetMinimumLevel(LogLevel.Trace));
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
             return builder.Build().RunAsync();
