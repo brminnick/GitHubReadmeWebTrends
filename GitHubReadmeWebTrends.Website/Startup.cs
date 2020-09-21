cd0d4efbb14f780c3b7c56f636afff6de30b2e34
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2020-09-21 15:22:55 -0700

    Remove Unused Code

--- GitHubReadmeWebTrends.Website/Startup.cs
+++ GitHubReadmeWebTrends.Website/Startup.cs
@@ -1,11 +1,6 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using GitHubReadmeWebTrends.Common;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -50,10 +45,7 @@ namespace GitHubReadmeWebTrends.Website
 
             app.UseAuthorization();
 
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapRazorPages();
-            });
+            app.UseEndpoints(endpoints => endpoints.MapRazorPages());
         }
     }
 }
