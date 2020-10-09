db418d664cebe7fbbe5008ca7a901202b36d42e7
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2020-10-09 13:40:52 -0700

    Add ILogger

--- GitHubReadmeWebTrends.Console/Program.cs
+++ GitHubReadmeWebTrends.Console/Program.cs
@@ -1,12 +1,14 @@
-﻿using System;
+﻿using Microsoft.Extensions.Logging;
 
 namespace GitHubReadmeWebTrends.Console
 {
     class Program
     {
+        readonly ILogger _log = LoggerFactory.Create(builder => builder.AddConsole()).CreateLogger(nameof(Console));
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
         }
     }
 }
