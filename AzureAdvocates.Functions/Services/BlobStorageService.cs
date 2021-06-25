6aaf0cf248a593fd7eec2c3f3374d172ea20d517
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2021-06-25 11:17:24 -0700

    Resolve Build Errors

--- AzureAdvocates.Functions/Services/BlobStorageService.cs
+++ AzureAdvocates.Functions/Services/BlobStorageService.cs
@@ -2,8 +2,8 @@ using System;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Collections.Generic;
-using Microsoft.WindowsAzure.Storage.Blob;
 using Newtonsoft.Json;
+using Microsoft.Azure.Storage.Blob;
 
 namespace AzureAdvocates.Functions
 {
